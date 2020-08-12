efea301f94dc042bf58ee2d796d52d073a09a8cb
Julio César <53662692+JulioMartinez31@users.noreply.github.com>
2020-08-12 12:52:32 -0500

    Update ProgrammingAssigment2_cachematrix.R

--- ProgrammingAssigment2_cachematrix.R
+++ ProgrammingAssigment2_cachematrix.R
@@ -1,15 +1,51 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+library(matlib)
+
+makeCacheMatrix <- function(x = matrix()){
+  if (ncol(x)==nrow(x) && det(x)!=0) {
+    m <- NULL
+    set<-function(y){
+      x <<- y
+      m <<- NULL
+    }
+    get<-function() x
+    setinverse <- function() m <<- solve(x)
+    getinverse<-function() m
+    list(set=set,
+         get=get,
+         setinverse=setinverse,
+         getinverse=getinverse)
+    
+  }else{
+    return(message("This matrix is not invertible"))
+  }
+}
 
 makeCacheMatrix <- function(x = matrix()) {
 
+## this function find the inverse of a matrix
+  cacheSolve <- function(x, ...) {
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+      message("getting cached data")
+      return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
+  }
 }
 
 
-## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+#####  Example 
+
+m<-makeCacheMatrix(matrix(c(1,2,1,2,2,1,3,1,2),ncol=3,byrow=TRUE))
+m$get() # get the matrix 'm'
+m$setinverse()   # Set inverse of a matrix 'm'
+m$getinverse()   ## Return a matrix that is the inverse of 'm'
+
